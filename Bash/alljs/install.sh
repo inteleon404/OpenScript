2a752d59bd915c07ef442560eb59adf66495524b
𝙸𝙽𝚃𝙴𝙻𝙴𝙾𝙽𝟺𝟶𝟺 <182798040+inteleon404@users.noreply.github.com>
2026-09-16 05:52:55 +0600

    Add installation script for various tools
    This script installs several tools including waybackurls, httpx, subjs, getjs, katana, and Cariddi.

---
+++ Bash/alljs/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#installing  waybackurls
+go install github.com/tomnomnom/waybackurls@latest
+
+#installing httpx 
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+# installing subjs
+
+go install -v github.com/lc/subjs@latest
+
+# installing getjs
+go install github.com/003random/getJS@latest
+
+
+
+#installing katana
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+
+#installing Cariddi
+go install -v github.com/edoardottt/cariddi/cmd/cariddi@latest
